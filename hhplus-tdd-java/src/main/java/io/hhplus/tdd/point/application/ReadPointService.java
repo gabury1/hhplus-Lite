71b8cb1c0aceeb3c5933e258eebf6298bd6cd152
@LYST <gabury1@gmail.com>
2025-09-01 01:15:44 +0900

    Feat: #1 포인트 조회 구현

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/application/ReadPointService.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/application/ReadPointService.java
@@ -5,8 +5,14 @@ import io.hhplus.tdd.point.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service @RequiredArgsConstructor
 public class ReadPointService {
 
+    private final UserPointTable userPointTable;
 
+
+    public UserPoint getUserPoint(long userId) {
+
+        return userPointTable.selectById(userId);
+    }
 }
